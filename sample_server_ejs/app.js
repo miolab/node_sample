220951d0295482c9d2113b41f48d5d4f9d620f7c
im <poson_hemp@yahoo.co.jp>
2020-04-28 18:02:12 +0900

    add function: POST form

--- sample_server_ejs/app.js
+++ sample_server_ejs/app.js
@@ -1,25 +1,42 @@
 const http = require("http"),
   fs = require("fs"),
-  ejs = require("ejs");
+  ejs = require("ejs"),
+  qs = require("querystring");
 const setting = require("./setting");
 console.log(setting);
 
 const server = http.createServer();
 const template = fs.readFileSync(__dirname + "/public_html/index.ejs", "utf-8");
 
-let num = 0;
+// article posts
+const posts = [];
 
-server.on("request", function (req, res) {
-  num++;
+function renderForm(posts, res) {
   const data = ejs.render(template, {
-    title: "Sample ejs rendering.",
+    title: "Short message memorizer",
     content: "<strong>Node.js</strong> simple server.",
-    num: num,
+    posts: posts,
   });
 
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write(data);
   res.end();
+}
+
+server.on("request", function (req, res) {
+  if (req.method === "POST") {
+    req.data = "";
+    req.on("data", function (chunk) {
+      req.data += chunk;
+    });
+    req.on("end", function () {
+      const query = qs.parse(req.data);
+      posts.push(query.name);
+      renderForm(posts, res);
+    });
+  } else {
+    renderForm(posts, res);
+  }
 });
 
 server.listen(setting.port, setting.hostname);
