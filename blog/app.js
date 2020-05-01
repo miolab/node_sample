15d415e436ec0d2bd161490319c13bf66274d609
im <poson_hemp@yahoo.co.jp>
2020-05-01 21:20:29 +0900

    prepared front-mock

---
+++ blog/app.js
@@ -0,0 +1,34 @@
+const express = require("express"),
+  app = express(),
+  port = 3000,
+  post = require("./routes/post");
+
+const bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+// for Dev
+const logger = require("morgan");
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+// Middleware:
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(methodOverride());
+
+// for Dev; Logger
+app.use(logger("dev"));
+
+// Routing
+app.get("/", post.index);
+// app.get("/posts/new", post.new);
+// app.post("/posts/create", post.create);
+// app.get("/posts/:id", post.show);
+// app.get("/posts/:id/edit", post.edit);
+// app.put("/posts/:id/edit", post.update);
+// app.delete("/posts/:id/", post.destroy);
+
+app.listen(port, () => {
+  console.log("Server starting;", `http://localhost:${port}`);
+});
---
+++ blog/routes/post.js
@@ -0,0 +1,9 @@
+let posts = [
+  { title: "title_0", body: "body_0" },
+  { title: "title_1", body: "body_1" },
+  { title: "title_2", body: "body_2" },
+];
+
+exports.index = function (req, res) {
+  res.render("posts/index", { posts: posts });
+};
