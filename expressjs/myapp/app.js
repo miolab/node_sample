0c288b568ae3b5f367512344d642e0c9a957429b
im <poson_hemp@yahoo.co.jp>
2020-05-01 12:38:42 +0900

    add: middleware static public/

--- expressjs/myapp/app.js
+++ expressjs/myapp/app.js
@@ -2,6 +2,10 @@ const express = require("express"),
   app = express(),
   port = 3000;
 
+// File read (use middleware; file in public/)
+// e.g) http://localhost:3000/test2.txt
+app.use(express.static(__dirname + "/public"));
+
 // Front page
 app.get("/", (req, res) => {
   res.send("Hello express, im.");
@@ -25,6 +29,13 @@ app.get("/users/:name?", (req, res) => {
 app.get("/items/:id([0-9]+)", (req, res) => {
   res.send("Item number, " + req.params.id);
 });
+
+/* File read (Direct read)
+    e.g) http://localhost:3000/test.txt
+app.get("/test.txt", (req, res) => {
+  res.sendfile(__dirname + "/public/test.txt");
+});
+*/
 
 app.listen(port, () => {
   console.log("Server starting;", `http://localhost:${port}`);
