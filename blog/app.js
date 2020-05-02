bd7e0e9213813f444dedc08a2ca955309701680e
im <poson_hemp@yahoo.co.jp>
2020-05-02 13:40:49 +0900

    add function: EDIT & DELETE

--- blog/app.js
+++ blog/app.js
@@ -15,7 +15,15 @@ app.set("view engine", "ejs");
 // Middleware:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(methodOverride());
+app.use(
+  methodOverride((req, res) => {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // for Dev; Logger
 app.use(logger("dev"));
@@ -25,9 +33,9 @@ app.get("/", post.index);
 app.get("/posts/:id([0-9]+)", post.show);
 app.get("/posts/new", post.new);
 app.post("/posts/create", post.create);
-// app.get("/posts/:id/edit", post.edit);
-// app.put("/posts/:id/edit", post.update);
-// app.delete("/posts/:id/", post.destroy);
+app.get("/posts/:id/edit", post.edit);
+app.put("/posts/:id", post.update);
+app.delete("/posts/:id/", post.destroy);
 
 app.listen(port, () => {
   console.log("Server starting;", `http://localhost:${port}`);
--- blog/routes/post.js
+++ blog/routes/post.js
@@ -13,6 +13,20 @@ exports.show = function (req, res) {
 exports.new = function (req, res) {
   res.render("posts/new");
 };
+exports.edit = function (req, res) {
+  res.render("posts/edit", { post: posts[req.params.id], id: req.params.id });
+};
+exports.update = function (req, res) {
+  posts[req.body.id] = {
+    title: req.body.title,
+    body: req.body.body,
+  };
+  res.redirect("/");
+};
+exports.destroy = function (req, res) {
+  posts.splice(req.body.id, 1);
+  res.redirect("/");
+};
 exports.create = function (req, res) {
   const post = {
     title: req.body.title,
